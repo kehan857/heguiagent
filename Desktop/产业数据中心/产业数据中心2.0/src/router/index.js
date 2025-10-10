ef2754222dc1f7dfbf224c672ecca6ff5d781d0a
kehan857 <kehan857@gmail.com>
2025-10-10 16:13:55 +0800

    修复路由和404页面中的旧仓库路径引用

--- Desktop/产业数据中心/产业数据中心2.0/src/router/index.js
+++ Desktop/产业数据中心/产业数据中心2.0/src/router/index.js
@@ -92,7 +92,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.NODE_ENV === 'production' ? '/chanyeshujuzhongxin4.0/' : '/'),
+  history: createWebHistory(process.env.NODE_ENV === 'production' ? '/tianyun-data-center-vue/' : '/'),
   routes
 })
 
